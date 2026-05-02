e78e620fd29e61aef128c33b07eb7fdd163f2733
silven-mohan <silven667@gmail.com>
2026-05-02 13:18:48 +0530

    Added: Implementation of print() function.

---
+++ learn/print.py
@@ -0,0 +1,5 @@
+# Implementation of all of the features of the print() function:
+
+print("Hello, Everyone")
+print("HOD", "VC", "Asst. Prof of CSE ", sep = ', ', end = '')
+print("You were warm welcomed...")
