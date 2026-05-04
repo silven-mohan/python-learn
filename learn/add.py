2472fb4aa4d4bfa5fbacbd39dfef951807aa4e6d
silven-mohan <silven667@gmail.com>
2026-05-04 12:51:37 +0530

    Added: Implementation of the addition operation.

---
+++ learn/add.py
@@ -0,0 +1,5 @@
+# Addition of Two Numbers:
+
+a, b = map(int, input("Enter any two numbers: ").split())   # Takes two numbers from the users
+
+print(f"The Addition of {a} and {b} is: {a + b}")       # print the sum of the two numbers
