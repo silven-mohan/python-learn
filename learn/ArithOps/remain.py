e4a7e7530e091da287bdc3d2f630592f32392247
silven-mohan <silven667@gmail.com>
2026-05-04 13:28:19 +0530

    Added: Implementation of Modulus operation.

---
+++ learn/ArithOps/remain.py
@@ -0,0 +1,5 @@
+# Remainder of any two numbers:
+
+a, b = map(int, input("Enter any two numbers: ").split())   # Takes any two numbers from the user.
+
+print(f"The Remainder when {a} and {b} are divided is: {a % b}")
