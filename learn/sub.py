96392374e2114adac219baf65e33f0741a3cc61f
silven-mohan <silven667@gmail.com>
2026-05-04 12:54:45 +0530

    Added: Implementation of the subtraction operation.

---
+++ learn/sub.py
@@ -0,0 +1,5 @@
+# Subtraction of Two Numbers:
+
+a, b = map(int, input("Enter any two numbers: ").split())   # Takes any two numbers from the user
+
+print(f"The Subtraction of {a} and {b} is: {a - b}")    # Prints the subtraction of the two given numbers.
