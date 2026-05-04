809f8a0373e0c2ac6ed2674312bd8473a4b6982e
silven-mohan <silven667@gmail.com>
2026-05-04 13:33:24 +0530

    Added: Implementation of Exponentation op.

---
+++ learn/ArithOps/exp.py
@@ -0,0 +1,6 @@
+# Exponentation Implementation:
+
+base = int(input("Enter the base: "))       # Taking the base value
+exp = int(input("Enter the exponent: "))        # Taking the exponent value.
+
+print(f"{base} ^ {exp} = {base ** exp}")    # Prints the value.
