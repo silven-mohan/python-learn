f7cb796577901e8d0cad9e2a528da7625d4861bf
silven-mohan <silven667@gmail.com>
2026-05-04 13:22:20 +0530

    Added: Implementation of the division operation.

---
+++ learn/ArithOps/div.py
@@ -0,0 +1,5 @@
+# Division of any two numbers:
+
+a, b = map(int, input("Enter any two numbers: ").split())   # Takes any two numbers from the user.
+
+print(f"The Division of {a} and {b} is: {a / b}")
--- learn/ArithOps/mult.py
+++ learn/ArithOps/mult.py
@@ -2,4 +2,4 @@
 
 a, b = map(int, input("Enter any two numbers: ").split())   # Takes any two numbers from the user.
 
-print(f"The Multiplication of {a} and {b} is: {a * b}")   # Prints the multiplication of the given two numbers. 
+print(f"The Multiplication of {a} and {b} is: {a * b}")   # Prints the multiplication of the given two numbers.
