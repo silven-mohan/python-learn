6a8d6a78f9248c19b5d82c9377b55d157f00d3dc
silven-cslab <silvencslab@gmail.com>
2026-05-12 08:49:55 +0000

    Added: Program that prints ascii values and codes for the alphabets.

---
+++ learn/ascii.py
@@ -0,0 +1,12 @@
+# ASCII Values
+# This program prints all of the ascii codes and their values of all alphabets.
+
+i = 65
+while i <= 90:
+    print("%d : %c" % (i, i))
+    i += 1
+
+i = 97
+while i <= 122:
+    print("%d : %c" % (i, i))
+    i += 1
